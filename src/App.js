c88ebddc18ce26100218e3fcaa07b1632fa354a3
Angelica <angelica.wilson93@gmail.com>
2021-05-19 11:42:35 -0500

    install react-router-dom, add/ h1 and light style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>Movies</h1>
-      </header>
-    </div>
+    // <div>
+    //   <header>
+    //     <h1>Movie Directory</h1>
+    //   </header>
+    // </div>
+    
+    <Switch>
+      <Route exact path='/'>
+        <div>
+          <header>
+            <h1>Movie Directory</h1>
+          </header>
+        </div>
+      </Route>
+    </Switch>
   );
 }
 
